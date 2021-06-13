148de3d88a44c0934923b3c94962c79bd6fdd478
Linus Yong <>
2021-06-13 16:57:34 +0000

    can run in replit.com

--- app.py
+++ app.py
@@ -10,3 +10,8 @@ def home():
         n = 5
         result = m.p(5)
         return render_template('index.html', result=result)
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=8080)
+
+# app.run(host='0.0.0.0', port=8080)
